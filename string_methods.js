d423e8e7a3198ec51e07be9e3a88d1326d995efa
Patricia Bettio <pfbettio@gmail.com>
2020-02-10 18:31:08 +0100

    questions

---
+++ string_methods.js
@@ -0,0 +1,54 @@
+//1- Find the unicode code for santa = "🎅"; 
+
+const santa =  "🎅";
+console.log(santa.substring(0, 6))
+
+ 
+
+//Check if a filename ends in ".jpg" or not 
+
+ 
+
+ 
+
+ 
+
+//Check if a name has a hyphen in it 
+
+ 
+
+//Find the end of the first name in a full name 
+
+ 
+
+//Find the beginning of the last name in a full name 
+
+ 
+
+//Figure out if "Aalborg" is sorted before or after "Alerup" in danish 
+
+ 
+
+//Add dots to a string, so it becomes at least 20 characters long 
+
+ 
+
+//Add spaces before a text, so it is at least 10 characters 
+
+ 
+
+//Replace a string of any number of characters, with the same number of *s 
+
+//Change all "å" in a text to "aa" 
+
+ 
+
+//Remove any double- or triple- spaces in a text 
+
+//Get the last 10 characters of a name 
+
+//Separate a comma-separated list into an array of individual items 
+
+//Check out if an url begins with "http://" or "https://" 
+
+ 
