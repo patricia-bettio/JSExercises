073088e717237e1a8659c1bdcf271d66072f4eb4
Patricia Bettio <pfbettio@gmail.com>
2020-02-10 18:26:47 +0100

    last capt exercise completed

--- capitalization.js
+++ capitalization.js
@@ -21,3 +21,11 @@ console.log(name.substring(0,2)+name.substring(2,3).toUpperCase()+name.substring
 //OR using Lowercase
 
 console.log(name.substring(0,2).toUpperCase()+name.substring(2,4).toLowerCase()+name.substring(4,).toUpperCase())
+
+
+//modify 1st letter only
+
+console.log(name.substring(0,1).toUpperCase()+name.substring(1,).toLowerCase())
+
+const anotherName = "richard"
+console.log(anotherName.substring(0,1).toUpperCase()+anotherName.substring(1,).toLowerCase())
