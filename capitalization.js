b43c71793d4347947e6a5d0c66d112e92cc91e34
Patricia Bettio <pfbettio@gmail.com>
2020-02-10 18:16:38 +0100

    capitalization completed

--- capitalization.js
+++ capitalization.js
@@ -1,3 +1,23 @@
 const name = "patricia";
 console.log(name)
 
+//grab the 3rd letter
+const letter3 = name.substring(2, 3);
+console.log(letter3)
+
+//make the 3rd letter Uppercase
+const newLetter = letter3.toUpperCase();
+console.log(newLetter)
+
+//final combination
+console.log(name.substring(0,2)+newLetter+name.substring(3,))
+
+//OR
+
+//shortway
+
+console.log(name.substring(0,2)+name.substring(2,3).toUpperCase()+name.substring(3,))
+
+//OR using Lowercase
+
+console.log(name.substring(0,2).toUpperCase()+name.substring(2,4).toLowerCase()+name.substring(4,).toUpperCase())
