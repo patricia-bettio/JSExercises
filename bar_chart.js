d88844a21361a2d83628f6a7d15497a23c931a5a
Patricia Bettio <pfbettio@gmail.com>
2020-02-15 18:52:45 +0100

    fix bar style js

--- bar_chart.js
+++ bar_chart.js
@@ -1,26 +1,54 @@
  "use strict";
-window.addEventListener("DOMContentLoaded", loop);
+window.addEventListener("DOMContentLoaded", init);
+
+function init(){
+  loop();
+ setHeight();
+}
 
 let array = [];
 
 function loop(){
 
   function getNumbers(){
-    return Math.floor(Math.random() * 38);
+    return Math.floor(Math.random() * 32);
   }
 
   const queueSize = getNumbers();
   //console.log(queueSize)
  
 
-if (array.length < 10) {
+if (array.length < 5) {
   array.unshift(queueSize);
-    setTimeout(loop, 300)
+    setTimeout(loop, 400)
     console.clear()
     console.log(array)
+    //for each new number in,:
+    setHeight();
+    
     
 } else {
   array.pop();
-  setTimeout(loop, 300)
+  setTimeout(loop, 400)
+  //console.log(array[0])
 }
   }
+  
+
+
+ function setHeight(){   
+//find the current number: console.log(array[0])
+console.log(array[0])
+
+const oneBar = document.querySelector("#bars > div:nth-child(1)");
+oneBar.style.height="100px";
+//oneBar.style.setProperty("--barHeight", array[0]); 
+
+
+//set the height of the bar:
+//oneBar.style.height="100px";
+//oneBar.style.setPrpperty("--barHeight", oneBar);
+
+
+ }
+ 
