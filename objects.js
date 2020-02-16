accf8df65435afa58b5b2ccd19788a3f88f9ed04
Patricia Bettio <pfbettio@gmail.com>
2020-02-16 10:55:32 +0100

    basic structure

---
+++ objects.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let object = {
+    firstName = "Patricia";
+    age = 30
+    student = false
+};
+console.log(object)
+console.log(`${object.firstName} is ${object.age} years old`)
