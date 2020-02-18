00eb38c06d972c932baa373989ff134b35181a33
Patricia Bettio <pfbettio@gmail.com>
2020-02-18 11:51:23 +0100

    color palete

--- exerccolor.js
+++ exerccolor.js
@@ -1,16 +1,41 @@
 "use strict"
 
-
-function greeting (firstName){
     
-    console.log("tetsing");
-    //let firstName = "Patricia";
-    console.log(`Hello ${firstName}`)
-    //console.log(greeting("any name"));
-    //const value = `Hello ${firstName}`;
-    //const result = greeting("any name")
-    //return value;
+/* function greeting(firstName){
 
+    let value = `Hello ${firstName}`;
+
+    return value;
+
+    } */
+
+ 
+    //this is the function that shopws result in console.log
+    function getColorString(red, green, blue){
+
+        let value = `rgb(${red}, ${green}, ${blue})`;
+        return value;
+        
     }
 
-  
+    //this is the function that sets the random number
+
+function randomColor(){
+    
+    const red = Math.floor(Math.random() * 255);
+
+    const green = Math.floor(Math.random() * 255);
+
+    const blue = Math.floor(Math.random() * 255);
+
+    
+
+    return getColorString(red, green, blue);
+
+}
+
+
+//console.log(value)
+function randomBackground(){
+document.querySelector("body").style.backgroundColor = randomColor();
+}
